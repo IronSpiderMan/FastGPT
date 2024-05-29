9da003aa7a82d6e8515f7b9390f7d8a0ac593790
Zack <sockwz@163.com>
2024-05-29 14:14:47 +0800

    修复bug

--- projects/app/src/pages/account/index.tsx
+++ projects/app/src/pages/account/index.tsx
@@ -100,13 +100,13 @@ const Account = ({ currentTab }: { currentTab: `${TabEnum}` }) => {
     //   label: t('support.user.Manage'),
     //   id: TabEnum.manage
     // },
-    ...(userInfo?.username === 'root' || userInfo?.username === 'admin'?
-        [
+    ...(userInfo?.username === 'root' || userInfo?.username === 'admin'
+      ? [
           {
             icon: 'support/user/userLight',
             label: t('support.user.Manage'),
             id: TabEnum.manage
-          },
+          }
         ]
       : []),
     {
