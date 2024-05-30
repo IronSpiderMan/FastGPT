da0bb731de4e2410e813aa88ed27b168b52b26ca
Zack <sockwz@163.com>
2024-05-30 13:30:48 +0800

    修复bug

--- projects/app/src/pages/api/support/user/account/addUser.ts
+++ projects/app/src/pages/api/support/user/account/addUser.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import { jsonRes } from '@fastgpt/service/common/response';
 import { MongoUser } from '@fastgpt/service/support/user/schema';
 import { MongoTeamMember } from '@fastgpt/service/support/user/team/teamMemberSchema';
+import { MongoTeam } from '@fastgpt/service/support/user/team/teamSchema';
 import { connectToDatabase } from '@/service/mongo';
 import type { PostLoginProps } from '@fastgpt/global/support/user/api.d';
 import { hashStr } from '@fastgpt/global/common/string/tools';
@@ -14,10 +15,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   try {
     await connectToDatabase();
     const { username, password } = req.body as PostLoginProps;
-    console.log('add user...', username, password);
-    // if (!username || !password) {
-    //   throw new Error('缺少参数');
-    // }
     // 检测用户是否存在
     const exists = await MongoUser.findOne(
       {
@@ -28,28 +25,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     if (exists) {
       throw new Error('用户已注册');
     }
-
     const user = await MongoUser.create({
       username: username,
       password: hashStr(password)
     });
-    console.log('===========创建用户================');
-    console.log(user);
-    //       await MongoTeamMember.create(
-    // [
-    //   {
-    //     teamId: insertedId,
-    //     userId,
-    //     name: 'Owner',
-    //     role: TeamMemberRoleEnum.owner,
-    //     status: TeamMemberStatusEnum.active,
-    //     createTime: new Date(),
-    //     defaultTeam: true
-    //   }
-    // ],
-    // { session }
+    const team = await MongoTeam.findOne({ name: 'My Team' });
     await MongoTeamMember.create({
-      teamId: '65ee937be73242bdc4a05982',
+      teamId: team.teamId,
       userId: user._id,
       name: 'Admin',
       role: TeamMemberRoleEnum.admin,
